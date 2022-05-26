af748c5b5229c0386f7e034221ab12013c971b8c
SammyMERAZGA <sammy.merazga@gmail.com>
2022-05-26 10:51:05 +0200

    Background done

---
+++ SwiftUI-Weather/ContentView.swift
@@ -0,0 +1,25 @@
+ //
+//  ContentView.swift
+//  SwiftUI-Weather
+//
+//  Created by Sammy Merazga on 26/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            LinearGradient(gradient: Gradient(colors: [.blue, .white]),
+                           startPoint: .topLeading,
+                           endPoint: .bottomTrailing)
+                .edgesIgnoringSafeArea(.all)
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
